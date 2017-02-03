d36402aa94b9743611c4bc4a5b4a011c8b9da1e3
Unknown <ruizangelica5525@gmail.com>
2017-02-02 17:42:38 -0700

    Adding comments to code and fixing it up in notepad for my eyes

--- DGM 1600/Assets/Move.cs
+++ DGM 1600/Assets/Move.cs
@@ -4,20 +4,42 @@ using System.Collections;
 public class Move : MonoBehaviour {
 
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			gameObject.transform.Translate (-.5f, 0, 0);
-		}
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			gameObject.transform.Translate (.5f, 0, 0);
-		}
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			gameObject.transform.Translate (0, .5f, 0);
+	
+void Update () {
+		
+if (Input.GetKey (KeyCode.LeftArrow)) {
+			
+	gameObject.transform.Translate (-.2f, 0, 0);
 
-		}
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			gameObject.transform.Translate (0, -.5f, 0);
+		// this lets the sphere move to the left at a speed of -.2		
+}
+		
+if (Input.GetKey (KeyCode.RightArrow)) {
+			
+	gameObject.transform.Translate (.2f, 0, 0);
 
-		}
-	}
+		// this lets the sphere move to the right at the speed of .2	
+}
+		
+if (Input.GetKey (KeyCode.UpArrow)) {
+			
+	gameObject.transform.Translate (0, .2f, 0);
+
+
+		// this lets the sphere move up at the speed of .2
+}
+		
+if (Input.GetKey (KeyCode.DownArrow)) {
+			
+	gameObject.transform.Translate (0, -.2f, 0);
+
+
+		/* this lets the shpere move down at the speed of -.2
+		 * side note: unity project files didn't update on gitKraken
+		 * as expected will update as soon as possiable
+		*/
+}
+		 
+}
+
 }
