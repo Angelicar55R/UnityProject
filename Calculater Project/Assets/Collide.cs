281d91966fd35732076e97e7892d89edd7738a4f
Angelica Ruiz <ruizangelica5525@gmail.com>
2017-04-25 11:06:17 -0600

    collider

---
+++ Calculater Project/Assets/Collide.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+
+public class Collide : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other)
+	{
+		print (other.gameObject);
+		Destroy (other.gameObject);
+		GameEnd ();
+	}
+
+	void GameEnd ()
+
+	{
+		print("Game Over");
+	
+	}
+}
