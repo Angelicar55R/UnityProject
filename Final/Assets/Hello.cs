97f4daa74e9ecc54d1207a3d74b070f77002a662
Angelica Ruiz <ruizangelica5525@gmail.com>
2017-05-02 10:59:13 -0600

    final

---
+++ Final/Assets/Hello.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Hello : MonoBehaviour {
+	public string phrase = "Hello My name is Angelica";
+	int time =10;
+
+	// Use this for initialization
+	void Start () {
+		welcomePhrase ();
+	}
+
+	void welcomePhrase() {
+		string welcome = phrase + time;
+		print (phrase + ", it is " + time + ":00 o'clock am.");
+			
+
+	}
+}
